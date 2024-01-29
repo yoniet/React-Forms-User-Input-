003359395d289075d6a17f8474057dc5d14a9655
YoniEtedgi <yonie812@gmail.com>
2024-01-29 10:17:17 +0200

    changes Signup Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Header from './components/Header.jsx';
 // import Signup from './components/Signup.jsx';
-import StateLogin from './components/StateLogin.jsx';
+import Signup from './components/Signup.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <StateLogin />
+        <Signup />
       </main>
     </>
   );
